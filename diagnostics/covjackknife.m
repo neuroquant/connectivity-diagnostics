aab1bb50d0ea6fcf1c79b444f3768fa864c17ce4
Manjari Narayan (VA) <manjari.narayan@gmail.com>
2016-08-19 16:21:59 -0700

    Fixed bug in switch statement parsing arguments

--- diagnostics/covjackknife.m
+++ diagnostics/covjackknife.m
@@ -6,34 +6,34 @@ function [Sigmab Sigma] = covjackknife(X,varargin)
 %  X		: Data matrix of size m x p x n, with following dimensions
 % 				(1 time, 2 space, 3 subjects)
 %  permdim  : [1 2 3] by default. Argument for permute function
-% 				permdim shuffles X to ensure that third dimension of X is jacknife dimension. 	
-%									  
-% Outputs:  									  
+% 				permdim shuffles X to ensure that third dimension of X is jacknife dimension.
+%
+% Outputs:
 % 	Sigmab 	: Jackknifed sample covariance of size p x p
-% 	Sigma	: Sample covariance of size p x p					
+% 	Sigma	: Sample covariance of size p x p
 %
 
   dim = length(size(X));
 
   switch nargin
+    case 1
+    	permdim = [1 2 3];
+	    useRobust = 0;
+
     case 2
-    	permdim = [1 2 3]; 
-	    useRobust = 0;
-		
-    case 3
 	  	permdim = varargin{1};
 	    useRobust = 0;
-		
-	case 4
+
+	case 3
 	  	permdim = varargin{1};
-		useRobust = varargin{2}; 
+		useRobust = varargin{2};
 	otherwise
     	disp('3 or more inputs not yet supported');
-      	permdim = [1 1 1]; 
+      	permdim = [1 2 3];
   end
-  X = permute(X,permdim);  
+  X = permute(X,permdim);
   [m p n] = size(X);
-  % Check that jackknife dimension is greater than 1   
+  % Check that jackknife dimension is greater than 1
   if(dim==3 & n==1)
     error('No samples in the 3rd matrix dimension to resample');
   end
@@ -50,18 +50,18 @@ function [Sigmab Sigma] = covjackknife(X,varargin)
       n_resamples = n;
     end
 
- Sigma = zeros(p,p,n); 
+ Sigma = zeros(p,p,n);
  for cc=1:n
 	 if(~useRobust)
-		 Sigma(:,:,cc) = corr(X(:,:,cc)); 
+		 Sigma(:,:,cc) = corr(X(:,:,cc));
 	 else
-		 tmpobj = GGM(X(:,:,cc)); 
+		 tmpobj = GGM(X(:,:,cc));
 		 tmpobj.tylerMLE();
 		 Sigma(:,:,cc) = tmpobj.Sigma;
 	 end
  end
 
- 
+
  Sigmab = zeros(p,p,n_resamples);
  all_n = [1:n_resamples];
   for r=1:n_resamples
